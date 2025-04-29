544c41507291f16fcdfccedeaed1f226d1404609
Abhishek Khati <abhishekkhati39@gmail.com>
2025-04-29 20:53:46 +0545

    set trust proxy in app configuration; remove unused Op import in VisitorController

--- src/controllers/visitor.controller.ts
+++ src/controllers/visitor.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import Visitor from "../database/models/visitor.model";
-import { Op } from "sequelize";
+
 class VisitorController {
   async trackVisitor(req: Request, res: Response): Promise<void> {
     try {
