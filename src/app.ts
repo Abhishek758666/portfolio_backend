425823bd5ca9a9813eae46d2465951dc9f13e9c4
Abhishek Khati <abhishekkhati39@gmail.com>
2025-05-06 15:15:22 +0545

    fix: update CORS configuration to use HTTPS for admin domain

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3001",
-      "http://admin.abhishekhati.com.np",
+      "https://admin.abhishekhati.com.np",
       "https://your-production-frontend.com",
     ],
     credentials: true,
